034799f6acda24c8f9c19fa4c0c1127479c39548
Danila_Goloshchapov <danila_goloshchapov@epam.com>
2020-09-23 23:44:23 +0300

    left equality tests added

--- joinwiz_core/src/main/scala/joinwiz/expression/equal.scala
+++ joinwiz_core/src/main/scala/joinwiz/expression/equal.scala
@@ -7,8 +7,6 @@ final case class LTCol2W[O, T] private[joinwiz] (wrapped: LTCol2[O, T])
 final case class RTCol2W[O, T] private[joinwiz] (wrapped: RTCol2[O, T])
 final case class LTCol2OptW[O, T] private[joinwiz] (wrapped: LTCol2[O, Option[T]])
 final case class RTCol2OptW[O, T] private[joinwiz] (wrapped: RTCol2[O, Option[T]])
-final case class ConstW[T] private[joinwiz] (wrapped: T)
-final case class OptConstW[T] private[joinwiz] (wrapped: Option[T])
 
 trait LowLevelEqualSyntax {
   import org.apache.spark.sql.functions.lit
@@ -17,15 +15,13 @@ trait LowLevelEqualSyntax {
   implicit def rtColToWrapper[O, T](col: RTCol2[O, T]): RTCol2W[O, T]               = RTCol2W[O, T](col)
   implicit def ltOptColToWrapper[O, T](col: LTCol2[O, Option[T]]): LTCol2OptW[O, T] = LTCol2OptW[O, T](col)
   implicit def rtOptColToWrapper[O, T](col: RTCol2[O, Option[T]]): RTCol2OptW[O, T] = RTCol2OptW[O, T](col)
-  implicit def constWrapper[T](const: T): ConstW[T]                                 = ConstW[T](const)
-  implicit def optConstWrapper[T](const: Option[T]): OptConstW[T]                   = OptConstW[T](const)
 
   implicit class LTCol2EqualSyntax[L, T](thisCol: LTCol2[L, T]) {
     def =:=[R](thatCol: LTCol2W[L, T]): Expr[L, R]    = expr[L, R](thisCol.column === thatCol.wrapped.column)((l, _) => thisCol(l) == thatCol.wrapped(l))
     def =:=[R](thatCol: RTCol2W[R, T]): Expr[L, R]    = expr[L, R](thisCol.column === thatCol.wrapped.column)((l, r) => thisCol(l) == thatCol.wrapped(r))
     def =:=[R](thatCol: LTCol2OptW[L, T]): Expr[L, R] = expr[L, R](thisCol.column === thatCol.wrapped.column)((l, _) => thatCol.wrapped(l) contains thisCol(l))
     def =:=[R](thatCol: RTCol2OptW[R, T]): Expr[L, R] = expr[L, R](thisCol.column === thatCol.wrapped.column)((l, r) => thatCol.wrapped(r) contains thisCol(l))
-    def =:=[R](const: ConstW[T]): Expr[L, R]          = expr[L, R](thisCol.column === lit(const))((l, _) => thisCol(l) == const.wrapped)
+    def =:=[R](const: T): Expr[L, R]          = expr[L, R](thisCol.column === lit(const))((l, _) => thisCol(l) == const)
   }
 
   implicit class RTCol2EqualSyntax[R, T](thisCol: RTCol2[R, T]) {
@@ -33,7 +29,7 @@ trait LowLevelEqualSyntax {
     def =:=[L](thatCol: RTCol2W[R, T]): Expr[L, R]    = expr[L, R](thisCol.column === thatCol.wrapped.column)((_, r) => thisCol(r) == thatCol.wrapped(r))
     def =:=[L](thatCol: RTCol2OptW[R, T]): Expr[L, R] = expr[L, R](thisCol.column === thatCol.wrapped.column)((_, r) => thatCol.wrapped(r) contains thisCol(r))
     def =:=[L](thatCol: LTCol2OptW[L, T]): Expr[L, R] = expr[L, R](thisCol.column === thatCol.wrapped.column)((l, r) => thatCol.wrapped(l) contains thisCol(r))
-    def =:=[L](const: ConstW[T]): Expr[L, R]          = expr[L, R](thisCol.column === lit(const))((_, r) => thisCol(r) == const)
+    def =:=[L](const: T): Expr[L, R]          = expr[L, R](thisCol.column === lit(const))((_, r) => thisCol(r) == const)
   }
 }
 
@@ -43,9 +39,9 @@ trait EqualSyntax extends LowLevelEqualSyntax {
   implicit class OptionalLTCol2EqualSyntax[L, T](thisCol: LTCol2[L, Option[T]]) {
     def =:=[R](thatCol: LTCol2W[L, T]): Expr[L, R]    = expr[L, R](thisCol.column === thatCol.wrapped.column)((l, _) => thisCol(l) contains thatCol.wrapped(l))
     def =:=[R](thatCol: RTCol2W[R, T]): Expr[L, R]    = expr[L, R](thisCol.column === thatCol.wrapped.column)((l, r) => thisCol(l) contains thatCol.wrapped(r))
-    def =:=[R](thatCol: LTCol2OptW[L, T]): Expr[L, R] = expr[L, R](thisCol.column === thatCol.wrapped.column)((l, _) => thatCol.wrapped(l) == thisCol.wrapped(l))
+    def =:=[R](thatCol: LTCol2OptW[L, T]): Expr[L, R] = expr[L, R](thisCol.column === thatCol.wrapped.column)((l, _) => thisCol.wrapped(l).isDefined && thatCol.wrapped(l) == thisCol.wrapped(l))
     def =:=[R](thatCol: RTCol2OptW[R, T]): Expr[L, R] = expr[L, R](thisCol.column === thatCol.wrapped.column)((l, r) => thatCol.wrapped(r).isDefined && thatCol.wrapped(r) == thisCol.wrapped(l))
-    def =:=[R](const: ConstW[T]): Expr[L, R]          = expr[L, R](thisCol.column === lit(const))((l, _) => thisCol(l) contains const.wrapped)
+    def =:=[R](const: T): Expr[L, R]          = expr[L, R](thisCol.column === lit(const))((l, _) => thisCol(l) contains const)
   }
 
   implicit class OptionalRTCol2EqualOptionSyntax[R, T](thisCol: RTCol2[R, Option[T]]) {
@@ -53,6 +49,6 @@ trait EqualSyntax extends LowLevelEqualSyntax {
     def =:=[L](thatCol: RTCol2W[R, T]): Expr[L, R]    = expr[L, R](thisCol.column === thatCol.wrapped.column)((_, r) => thisCol(r) contains thatCol.wrapped(r))
     def =:=[L](thatCol: RTCol2OptW[R, T]): Expr[L, R] = expr[L, R](thisCol.column === thatCol.wrapped.column)((_, r) => thatCol.wrapped(r) == thisCol.wrapped(r))
     def =:=[L](thatCol: LTCol2OptW[L, T]): Expr[L, R] = expr[L, R](thisCol.column === thatCol.wrapped.column)((l, r) => thatCol.wrapped(l) == thisCol.wrapped(r))
-    def =:=[L](const: ConstW[T]): Expr[L, R]          = expr[L, R](thisCol.column === lit(const))((_, r) => thisCol(r) contains const.wrapped)
+    def =:=[L](const: T): Expr[L, R]          = expr[L, R](thisCol.column === lit(const))((_, r) => thisCol(r) contains const)
   }
 }
--- joinwiz_core/src/test/scala/joinwiz/expression/EqualSyntaxTest.scala
+++ joinwiz_core/src/test/scala/joinwiz/expression/EqualSyntaxTest.scala
@@ -1,6 +1,6 @@
 package joinwiz.expression
 
-import joinwiz.{ApplyLTCol2, ApplyRTCol2}
+import joinwiz.{ApplyLTCol2, ApplyRTCol2, Expr}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
@@ -8,8 +8,8 @@ import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 class EqualSyntaxTest extends AnyFunSuite with Matchers {
   import joinwiz.syntax2._
 
-  case class A(pk: String = null, optional: Option[String] = None)
-  case class B(pk: String = null, optional: Option[String] = None)
+  case class A(pk: String = null, opt: Option[String] = None)
+  case class B(pk: String = null, opt: Option[String] = None)
 
   private val evaluate = (ApplyLTCol2[A], ApplyRTCol2[B])
 
@@ -26,27 +26,109 @@ class EqualSyntaxTest extends AnyFunSuite with Matchers {
 
   test("left T and right opt T can equal") {
     val evaluated = evaluate match {
-      case (left, right) => left(_.pk) =:= right(_.optional)
+      case (left, right) => left(_.pk) =:= right(_.opt)
     }
 
-    evaluated(A(pk = "matching"), B(optional = Some("matching"))) should be(true)
-    evaluated(A(pk = "matching"), B(optional = Some("non-matching"))) should be(false)
-    evaluated(A(pk = "matching"), B(optional = None)) should be(false)
+    evaluated(A(pk = "matching"), B(opt = Some("matching"))) should be(true)
+    evaluated(A(pk = "matching"), B(opt = Some("non-matching"))) should be(false)
+    evaluated(A(pk = "matching"), B(opt = None)) should be(false)
 
-    evaluated.expr.expr.toString() should be("('LEFT.pk = 'RIGHT.optional)")
+    evaluated.expr.expr.toString() should be("('LEFT.pk = 'RIGHT.opt)")
+  }
+
+  test("left T and left T can equal") {
+    val evaluated = evaluate match {
+      case (left, _) => left(_.pk).=:=[B](left(_.pk))
+    }
+
+    evaluated(A(pk = "matching"), B()) should be(true)
+
+    evaluated.expr.expr.toString() should be("('LEFT.pk = 'LEFT.pk)")
+  }
+
+  test("left T and left opt T can equal") {
+    val evaluated = evaluate match {
+      case (left, _) => left(_.pk).=:=[B](left(_.opt))
+    }
+
+    evaluated(A(pk = "matching", opt = Some("matching")), B()) should be(true)
+    evaluated(A(pk = "matching", opt = Some("non-matching")), B()) should be(false)
+    evaluated(A(pk = "matching", opt = None), B()) should be(false)
+
+    evaluated.expr.expr.toString() should be("('LEFT.pk = 'LEFT.opt)")
+  }
+
+  test("left T and const T can equal") {
+    val evaluated = evaluate match {
+      case (left, _) => left(_.pk).=:=[B]("matching")
+    }
+
+    evaluated(A(pk = "matching"), B()) should be(true)
+    evaluated(A(pk = "non-matching"), B()) should be(false)
+
+    evaluated.expr.expr.toString() should be("('LEFT.pk = matching)")
+  }
+
+  test("left opt T and right T can equal") {
+    val evaluated = evaluate match {
+      case (left, right) => left(_.opt) =:= right(_.pk)
+    }
+
+    evaluated(A(opt = Some("matching")), B(pk = "matching")) should be(true)
+    evaluated(A(opt = Some("matching")), B(pk = "non-matching")) should be(false)
+    evaluated(A(opt = Some("non-matching")), B(pk = "matching")) should be(false)
+    evaluated(A(opt = None), B(pk = "matching")) should be(false)
+
+    evaluated.expr.expr.toString() should be("('LEFT.opt = 'RIGHT.pk)")
   }
 
   test("left opt T and right opt T can equal") {
     val evaluated = evaluate match {
-      case (left, right) => left(_.optional) =:= right(_.optional)
+      case (left, right) => left(_.opt) =:= right(_.opt)
     }
 
-    evaluated(A(optional = Some("matching")), B(optional = Some("matching"))) should be(true)
-    evaluated(A(optional = Some("matching")), B(optional = Some("non-matching"))) should be(false)
-    evaluated(A(optional = Some("matching")), B(optional = None)) should be(false)
-    evaluated(A(optional = None), B(optional = None)) should be(false)
+    evaluated(A(opt = Some("matching")), B(opt = Some("matching"))) should be(true)
+    evaluated(A(opt = Some("matching")), B(opt = Some("non-matching"))) should be(false)
+    evaluated(A(opt = Some("matching")), B(opt = None)) should be(false)
+    evaluated(A(opt = None), B(opt = None)) should be(false)
 
-    evaluated.expr.expr.toString() should be("('LEFT.optional = 'RIGHT.optional)")
+    evaluated.expr.expr.toString() should be("('LEFT.opt = 'RIGHT.opt)")
+  }
+
+  test("left opt T and left T can equal") {
+    val evaluated = evaluate match {
+      case (left, _) => left(_.opt).=:=[B](left(_.pk))
+    }
+
+    evaluated(A(opt = Some("matching"), pk = "matching"), B()) should be(true)
+    evaluated(A(opt = Some("matching"), pk = "non-matching"), B()) should be(false)
+    evaluated(A(opt = Some("non-matching"), pk = "matching"), B()) should be(false)
+    evaluated(A(opt = None, pk = "matching"), B()) should be(false)
+
+    evaluated.expr.expr.toString() should be("('LEFT.opt = 'LEFT.pk)")
+  }
+
+  test("left opt T and left opt T can equal") {
+    val evaluated = evaluate match {
+      case (left, _) => left(_.opt).=:=[B](left(_.opt))
+    }
+
+    evaluated(A(opt = Some("matching")), B()) should be(true)
+    evaluated(A(opt = None), B()) should be(false)
+
+    evaluated.expr.expr.toString() should be("('LEFT.opt = 'LEFT.opt)")
+  }
+
+  test("left opt T and const T can equal") {
+    val evaluated = evaluate match {
+      case (left, _) => left(_.opt).=:=[B]("matching")
+    }
+
+    evaluated(A(opt = Some("matching")), B()) should be(true)
+    evaluated(A(opt = Some("non-matching")), B()) should be(false)
+    evaluated(A(opt = None), B()) should be(false)
+
+    evaluated.expr.expr.toString() should be("('LEFT.opt = matching)")
   }
 
 //
