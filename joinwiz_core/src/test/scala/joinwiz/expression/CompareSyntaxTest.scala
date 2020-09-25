a12bc713ce7f7a76854654b02c499135b6cd5cb9
Danila_Goloshchapov <danila_goloshchapov@epam.com>
2020-09-25 13:20:23 +0300

    right comparision done

--- joinwiz_core/src/test/scala/joinwiz/expression/CompareSyntaxTest.scala
+++ joinwiz_core/src/test/scala/joinwiz/expression/CompareSyntaxTest.scala
@@ -307,4 +307,301 @@ class CompareSyntaxTest extends AnyFunSuite with Matchers {
 
     evaluated.expr.expr.toString() should be("('LEFT.opt >= 1)")
   }
+
+  // tests for right
+  test("right T can be < than left T") {
+    val evaluated = evaluate match {
+      case (left, right) => right(_.pk) < left(_.pk)
+    }
+
+    evaluated(Left(pk = 1), Right(pk = 1)) should be(false)
+    evaluated(Left(pk = 1), Right(pk = 0)) should be(true)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.pk < 'LEFT.pk)")
+  }
+
+  test("right T can be <= than left T") {
+    val evaluated = evaluate match {
+      case (left, right) => right(_.pk) <= left(_.pk)
+    }
+
+    evaluated(Left(pk = 1), Right(pk = 1)) should be(true)
+    evaluated(Left(pk = 1), Right(pk = 0)) should be(true)
+    evaluated(Left(pk = 0), Right(pk = 1)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.pk <= 'LEFT.pk)")
+  }
+
+  test("right T can be > than left T") {
+    val evaluated = evaluate match {
+      case (left, right) => right(_.pk) > left(_.pk)
+    }
+
+    evaluated(Left(pk = 1), Right(pk = 1)) should be(false)
+    evaluated(Left(pk = 0), Right(pk = 1)) should be(true)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.pk > 'LEFT.pk)")
+  }
+
+  test("right T can be >= than left T") {
+    val evaluated = evaluate match {
+      case (left, right) => right(_.pk) >= left(_.pk)
+    }
+
+    evaluated(Left(pk = 1), Right(pk = 1)) should be(true)
+    evaluated(Left(pk = 0), Right(pk = 1)) should be(true)
+    evaluated(Left(pk = 0), Right(pk = -1)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.pk >= 'LEFT.pk)")
+  }
+
+  test("right T can be < than left opt T") {
+    val evaluated = evaluate match {
+      case (left, right) => right(_.pk) < left(_.opt)
+    }
+
+    evaluated(Left(opt = Some(1)), Right(pk = 1)) should be(false)
+    evaluated(Left(opt = Some(1)), Right(pk = 0)) should be(true)
+    evaluated(Left(opt = None), Right(pk = 1)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.pk < 'LEFT.opt)")
+  }
+
+  test("right T can be <= than left opt T") {
+    val evaluated = evaluate match {
+      case (left, right) => right(_.pk) <= left(_.opt)
+    }
+
+    evaluated(Left(opt = Some(1)), Right(pk = 1)) should be(true)
+    evaluated(Left(opt = Some(1)), Right(pk = 0)) should be(true)
+    evaluated(Left(opt = Some(-1)), Right(pk = 0)) should be(false)
+    evaluated(Left(opt = None), Right(pk = 0)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.pk <= 'LEFT.opt)")
+  }
+
+  test("right T can be > than left opt T") {
+    val evaluated = evaluate match {
+      case (left, right) => right(_.pk) > left(_.opt)
+    }
+
+    evaluated(Left(opt = Some(1)), Right(pk = 1)) should be(false)
+    evaluated(Left(opt = Some(0)), Right(pk = 1)) should be(true)
+    evaluated(Left(opt = None), Right(pk = 1)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.pk > 'LEFT.opt)")
+  }
+
+  test("right T can be >= than left opt T") {
+    val evaluated = evaluate match {
+      case (left, right) => right(_.pk) >= left(_.opt)
+    }
+
+    evaluated(Left(opt = Some(1)), Right(pk = 1)) should be(true)
+    evaluated(Left(opt = Some(0)), Right(pk = 1)) should be(true)
+    evaluated(Left(opt = Some(0)), Right(pk = -1)) should be(false)
+    evaluated(Left(opt = None), Right(pk = -1)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.pk >= 'LEFT.opt)")
+  }
+
+  test("right T can be < than const") {
+    val evaluated = evaluate match {
+      case (_, right) => right(_.pk).<[Left](1)
+    }
+
+    evaluated(Left(), Right(pk = 1)) should be(false)
+    evaluated(Left(), Right(pk = 0)) should be(true)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.pk < 1)")
+  }
+
+  test("right T can be <= than const") {
+    val evaluated = evaluate match {
+      case (_, right) => right(_.pk).<=[Left](1)
+    }
+
+    evaluated(Left(), Right(pk = 1)) should be(true)
+    evaluated(Left(), Right(pk = 0)) should be(true)
+    evaluated(Left(), Right(pk = 2)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.pk <= 1)")
+  }
+
+  test("right T can be > than const") {
+    val evaluated = evaluate match {
+      case (_, right) => right(_.pk).>[Left](1)
+    }
+
+    evaluated(Left(), Right(pk = 1)) should be(false)
+    evaluated(Left(), Right(pk = 2)) should be(true)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.pk > 1)")
+  }
+
+  test("right T can be >= than const") {
+    val evaluated = evaluate match {
+      case (_, right) => right(_.pk).>=[Left](1)
+    }
+
+    evaluated(Left(), Right(pk = 1)) should be(true)
+    evaluated(Left(), Right(pk = 2)) should be(true)
+    evaluated(Left(), Right(pk = 0)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.pk >= 1)")
+  }
+
+  test("right opt T can be < than right T") {
+    val evaluated = evaluate match {
+      case (left, right) => right(_.opt) < left(_.pk)
+    }
+
+    evaluated(Left(pk = 1), Right(opt = Some(1))) should be(false)
+    evaluated(Left(pk = 1), Right(opt = Some(0))) should be(true)
+    evaluated(Left(pk = 1), Right(opt = None)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.opt < 'LEFT.pk)")
+  }
+
+  test("right opt T can be <= than left T") {
+    val evaluated = evaluate match {
+      case (left, right) => right(_.opt) <= left(_.pk)
+    }
+
+    evaluated(Left(pk = 1), Right(opt = Some(1))) should be(true)
+    evaluated(Left(pk = 1), Right(opt = Some(0))) should be(true)
+    evaluated(Left(pk = -1), Right(opt = Some(0))) should be(false)
+    evaluated(Left(pk = -1), Right(opt = None)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.opt <= 'LEFT.pk)")
+  }
+
+  test("right opt T can be > than left T") {
+    val evaluated = evaluate match {
+      case (left, right) => right(_.opt) > left(_.pk)
+    }
+
+    evaluated(Left(pk = 1), Right(opt = Some(1))) should be(false)
+    evaluated(Left(pk = 0), Right(opt = Some(1))) should be(true)
+    evaluated(Left(pk = 0), Right(opt = None)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.opt > 'LEFT.pk)")
+  }
+
+  test("right opt T can be >= than left T") {
+    val evaluated = evaluate match {
+      case (left, right) => right(_.opt) >= left(_.pk)
+    }
+
+    evaluated(Left(pk = 1), Right(opt = Some(1))) should be(true)
+    evaluated(Left(pk = 0), Right(opt = Some(1))) should be(true)
+    evaluated(Left(pk = 0), Right(opt = Some(-1))) should be(false)
+    evaluated(Left(pk = 0), Right(opt = None)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.opt >= 'LEFT.pk)")
+  }
+
+  test("right opt T can be < than left opt T") {
+    val evaluated = evaluate match {
+      case (left, right) => right(_.opt) < left(_.opt)
+    }
+
+    evaluated(Left(opt = Some(1)), Right(opt = Some(1))) should be(false)
+    evaluated(Left(opt = Some(1)), Right(opt = Some(0))) should be(true)
+    evaluated(Left(opt = None), Right(opt = Some(0))) should be(false)
+    evaluated(Left(opt = None), Right(opt = None)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.opt < 'LEFT.opt)")
+  }
+
+  test("right opt T can be <= than left opt T") {
+    val evaluated = evaluate match {
+      case (left, right) => right(_.opt) <= left(_.opt)
+    }
+
+    evaluated(Left(opt = Some(1)), Right(opt = Some(1))) should be(true)
+    evaluated(Left(opt = Some(1)), Right(opt = Some(0))) should be(true)
+    evaluated(Left(opt = Some(-1)), Right(opt = Some(0))) should be(false)
+    evaluated(Left(opt = None), Right(opt = Some(0))) should be(false)
+    evaluated(Left(opt = None), Right(opt = None)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.opt <= 'LEFT.opt)")
+  }
+
+  test("right opt T can be > than left opt T") {
+    val evaluated = evaluate match {
+      case (left, right) => right(_.opt) > left(_.opt)
+    }
+
+    evaluated(Left(opt = Some(1)), Right(opt = Some(1))) should be(false)
+    evaluated(Left(opt = Some(0)), Right(opt = Some(1))) should be(true)
+    evaluated(Left(opt = None), Right(opt = Some(1))) should be(false)
+    evaluated(Left(opt = None), Right(opt = None)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.opt > 'LEFT.opt)")
+  }
+
+  test("right opt T can be >= than left opt T") {
+    val evaluated = evaluate match {
+      case (left, right) => right(_.opt) >= left(_.opt)
+    }
+
+    evaluated(Left(opt = Some(1)), Right(opt = Some(1))) should be(true)
+    evaluated(Left(opt = Some(0)), Right(opt = Some(1))) should be(true)
+    evaluated(Left(opt = Some(0)), Right(opt = Some(-1))) should be(false)
+    evaluated(Left(opt = None), Right(opt = Some(-1))) should be(false)
+    evaluated(Left(opt = None), Right(opt = None)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.opt >= 'LEFT.opt)")
+  }
+
+  test("right opt T can be < than const") {
+    val evaluated = evaluate match {
+      case (_, right) => right(_.opt).<[Left](1)
+    }
+
+    evaluated(Left(), Right(opt = Some(1))) should be(false)
+    evaluated(Left(), Right(opt = Some(0))) should be(true)
+    evaluated(Left(), Right(opt = None)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.opt < 1)")
+  }
+
+  test("right opt T can be <= than const") {
+    val evaluated = evaluate match {
+      case (_, right) => right(_.opt).<=[Left](1)
+    }
+
+    evaluated(Left(), Right(opt = Some(1))) should be(true)
+    evaluated(Left(), Right(opt = Some(0))) should be(true)
+    evaluated(Left(), Right(opt = Some(2))) should be(false)
+    evaluated(Left(), Right(opt = None)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.opt <= 1)")
+  }
+
+  test("right opt T can be > than const") {
+    val evaluated = evaluate match {
+      case (_, right) => right(_.opt).>[Left](1)
+    }
+
+    evaluated(Left(), Right(opt = Some(1))) should be(false)
+    evaluated(Left(), Right(opt = Some(2))) should be(true)
+    evaluated(Left(), Right(opt = None)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.opt > 1)")
+  }
+
+  test("right opt T can be >= than const") {
+    val evaluated = evaluate match {
+      case (_, right) => right(_.opt).>=[Left](1)
+    }
+
+    evaluated(Left(), Right(opt = Some(1))) should be(true)
+    evaluated(Left(), Right(opt = Some(2))) should be(true)
+    evaluated(Left(), Right(opt = Some(0))) should be(false)
+    evaluated(Left(), Right(opt = None)) should be(false)
+
+    evaluated.expr.expr.toString() should be("('RIGHT.opt >= 1)")
+  }
 }
